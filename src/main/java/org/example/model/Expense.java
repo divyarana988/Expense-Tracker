ae8fc7359bbc0f3f489909ffe799e598a2ab818e
Divya Rana <divyarana491@gmail.com>
2025-04-04 17:01:10 +0530

    added dtos

---
+++ src/main/java/org/example/model/Expense.java
@@ -0,0 +1,24 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "expense")
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String expenseName;
+    private String expenseCategory;
+    private BigDecimal expenseAmount;
+
+}
